47729ae5400862db4d755e7c69ca552914cfa79b
agustin9700 <ryuk10xd@gmail.com>
2024-07-27 13:22:49 -0300

    ajustando json

--- controllers/home.js
+++ controllers/home.js
@@ -1,16 +1,38 @@
 
-const fs = require('fs')
-module.exports=function(res,res){
+// const fs = require('fs')
+// module.exports=function(res,res){
 
     
+//     fs.readFile('resultadoResto15s.json', 'utf-8', (err, data) => {
+//         if (err) {
+//           console.error(err);
+//           res.status(500).send('Error al leer el archivo JSON');
+//           return;
+//         }
+//         const jsonData = JSON.parse(data);
+//         res.send(jsonData);
+//       });
+
+// }
+const fs = require('fs');
+
+module.exports = function(req, res) {
     fs.readFile('resultadoResto15s.json', 'utf-8', (err, data) => {
         if (err) {
-          console.error(err);
-          res.status(500).send('Error al leer el archivo JSON');
-          return;
+            console.error(err);
+            res.status(500).send('Error al leer el archivo JSON');
+            return;
         }
-        const jsonData = JSON.parse(data);
-        res.send(jsonData);
-      });
+        
+        let jsonData;
+        try {
+            jsonData = JSON.parse(data);
+        } catch (parseErr) {
+            console.error(parseErr);
+            res.status(500).send('Error al analizar el JSON');
+            return;
+        }
 
+        res.send(jsonData);
+    });
 }
--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,39 @@
 
-const fs = require('fs')
-module.exports=function(res,res){
+// const fs = require('fs')
+// module.exports=function(res,res){
 
     
+//     fs.readFile('resultadoResto.json', 'utf-8', (err, data) => {
+//         if (err) {
+//           console.error(err);
+//           res.status(500).send('Error al leer el archivo JSON');
+//           return;
+//         }
+//         const jsonData = JSON.parse(data);
+//         res.send(jsonData);
+//       });
+
+// }
+
+const fs = require('fs');
+
+module.exports = function(req, res) {
     fs.readFile('resultadoResto.json', 'utf-8', (err, data) => {
         if (err) {
-          console.error(err);
-          res.status(500).send('Error al leer el archivo JSON');
-          return;
+            console.error('Error al leer el archivo:', err);
+            res.status(500).send('Error al leer el archivo JSON');
+            return;
         }
-        const jsonData = JSON.parse(data);
-        res.send(jsonData);
-      });
+        
+        let jsonData;
+        try {
+            jsonData = JSON.parse(data);
+        } catch (parseErr) {
+            console.error('Error al analizar el JSON:', parseErr);
+            res.status(500).send('Error al analizar el archivo JSON');
+            return;
+        }
 
+        res.send(jsonData);
+    });
 }
